11c3662918ac988dd663b894288ff275f3c96bdf
Tim Condit <tcondit@gmail.com>
2020-05-12 09:18:02 -0700

    Display more posts per page

--- microblog/config.py
+++ microblog/config.py
@@ -15,4 +15,4 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["tim.condit@skillfox.com"]
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
