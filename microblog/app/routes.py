e535f547f8707f2c30d2f2139f7bb068e6d8c0bc
Tim Condit <tcondit@gmail.com>
2020-05-14 12:10:33 -0700

    Tagged the wrong string for translation

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -201,8 +201,8 @@ def explore():
     next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
     prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
     return render_template(
-        _("index.html"),
-        title="Explore",
+        "index.html",
+        title=_("Explore"),
         posts=posts.items,
         next_url=next_url,
         prev_url=prev_url,
