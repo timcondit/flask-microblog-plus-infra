aaad456b2e822c53b37564bbdf307c4d53554fac
Tim Condit <tcondit@gmail.com>
2020-05-15 15:06:08 -0700

    fixes #5

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -146,7 +146,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-    g.get_locale = str(get_locale())
+    g.locale = str(get_locale())
 
 
 @app.route("/edit_profile", methods=["GET", "POST"])
